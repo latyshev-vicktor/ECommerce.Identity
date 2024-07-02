a42d31983c9e88cc4e2e39b14b19a2ea6e58c134
viktor.latyshev <viktor.latyshev@samgau.com>
2024-07-02 09:31:14 +0500

    Добавил реализацию доменных событий

--- ECommerce.Domain/SeedWorks/BaseEntity.cs
+++ ECommerce.Domain/SeedWorks/BaseEntity.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace ECommerce.Domain.SeedWorks
 {
@@ -11,6 +7,19 @@ namespace ECommerce.Domain.SeedWorks
         public Guid Id { get; set; }
         public DateTimeOffset CreatedDate { get; private set; }
         public DateTimeOffset ModifyDate { get; private set; }
+
+        private List<INotification> _domainEvents = [];
+        public IReadOnlyList<INotification> DomainEvents => _domainEvents;
+
+        public void AddDomainEvent(INotification eventIntem)
+        {
+            _domainEvents.Add(eventIntem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
 
         public void MakeModify()
         {
